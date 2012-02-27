a0b2749076fe95584c8dd978eff30a62ff6b34c4
Daniel Naab <danielnaab@gmail.com>
2012-02-26 21:36:23 -0600

    Add fields to tree node admin

--- exercises/admin.py
+++ exercises/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from exercises.models import KhanExerciseTreeNode
 
-admin.site.register(KhanExerciseTreeNode)
+class KhanExerciseTreeNodeAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in KhanExerciseTreeNode._meta.fields]
+
+admin.site.register(KhanExerciseTreeNode, KhanExerciseTreeNodeAdmin)
