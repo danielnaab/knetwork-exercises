3d0e4f650576583f32fb3979a261c20f3b764198
Nima Gardideh <nima@halfmoon.ws>
2012-03-02 16:03:33 -0500

    Added embeding of excercises.

--- exercises/urls.py
+++ exercises/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import redirect_to
 from django.contrib import admin
 
 from exercises import views
@@ -8,6 +9,13 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.menu, name='exercises_menu'),
+    url(r'^css/(?P<id>.+)', redirect_to, {'url': '/static/khan-exercises/css/%(id)s'}),
+    url(r'^js/(?P<id>.+)', redirect_to, {'url': '/static/khan-exercises/js/%(id)s'}),
+    url(r'^utils/(?P<id>.+)', redirect_to, {'url': '/static/khan-exercises/utils/%(id)s'}),
+    url(r'^khan-exercise.js(?P<id>.+)', redirect_to, {'url': '/static/khan-exercises/khan-exercise.js%(id)s'}),
+    url(r'^jquery(?P<id>.+)', redirect_to, {'url': '/static/khan-exercises/jquery%(id)s'}),
+    url(r'^exercises-packed/(?P<id>.+)', redirect_to, {'url': '/static/khan-exercises/exercises-packed/%(id)s'}),
+    url(r'^build/(?P<id>.+)', redirect_to, {'url': '/static/khan-exercises/build/%(id)s'}),
 )
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
